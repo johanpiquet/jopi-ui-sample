46f7716976e65dfeed89b8ace4a1e76307668dac
Johan PIQUET <johan.piquet@gmail.com>
2025-10-01 08:43:20 +0200

    * setCurrentModuleInfo replaced by getModuleServerInitContent

--- src/modules/moduleA/jopiGetModuleInfo.tsx
+++
@@ -1,9 +0,0 @@
-// The role of this file is to INSTALL the module FILE once deployed.
-// It's executed by the tools jopi module install dirToScan
-
-import {setCurrentModuleInfo} from "jopi-rewrite";
-
-setCurrentModuleInfo({
-    //moduleName: "jopi.moduleA",
-    moduleTitle: "Module A"
-});
--- src/modules/moduleA/serverInit.tsx
+++ src/modules/moduleA/serverInit.tsx
@@ -1,3 +1,12 @@
 // The role of this file, is to initialize the module once mounted.
 
+import {getModuleServerInitContent} from "jopi-rewrite";
+
+const modInit = getModuleServerInitContent();
+
+modInit.setModuleInfo({
+    //moduleName: "jopi.moduleA",
+    moduleTitle: "Module A"
+});
+
 console.log('Module A initialized');
