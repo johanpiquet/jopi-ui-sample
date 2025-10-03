7e6d53676f97fece4a2ef2e755c7ea39152edc01
Johan PIQUET <johan.piquet@gmail.com>
2025-10-03 15:56:59 +0200

    useUserInfos now uses a store internally

--- src/modules/adminLayout/routes/user/$id/index.page.tsx
+++
@@ -1,9 +0,0 @@
-import {usePageParams} from "jopi-rewrite-ui";
-
-export default function() {
-    let params = usePageParams();
-
-    return <div>
-        Details page for user {params.id}
-    </div>;
-}
