af0f15e5b0bec6457ff77e364b1bbb8b4957cc22
Johan PIQUET <johan.piquet@gmail.com>
2025-10-08 19:19:19 +0200

    getRouteServerContext mechanism remplacer by a safe one

--- src/modules/adminLayout/routes/login/index.server.tsx
+++ src/modules/adminLayout/routes/login/index.server.tsx
@@ -1,14 +1,14 @@
-import {type LoginPassword, getRouteServerContext} from "jopi-rewrite";
+import {type LoginPassword, RouteServerContext} from "jopi-rewrite";
 
-let ctx = getRouteServerContext();
+export default function(ctx: RouteServerContext) {
+    ctx.onPOST(async req => {
+        const data = await req.getReqData({ignoreUrl: true});
+        const authResult = await req.tryAuthWithJWT(data as LoginPassword);
 
-ctx.onPOST(async req => {
-    const data = await req.getReqData({ignoreUrl: true});
-    const authResult = await req.tryAuthWithJWT(data as LoginPassword);
+        if (!authResult.isOk) console.log("Auth failed");
 
-    if (!authResult.isOk) console.log("Auth failed");
-
-    // Will automatically set a cookie.
-    // It why we don't core of the details here.
-    return req.jsonResponse({isOk: authResult && authResult.isOk});
-});
+        // Will automatically set a cookie.
+        // It why we don't core of the details here.
+        return req.jsonResponse({isOk: authResult && authResult.isOk});
+    });
+}
--- src/modules/demoContent/routes/features/forms/index.server.tsx
+++ src/modules/demoContent/routes/features/forms/index.server.tsx
@@ -1,16 +1,16 @@
-import {getRouteServerContext} from "jopi-rewrite";
 import {formSchema} from "./schema.ts";
 import {nTimer} from "jopi-node-space";
+import {RouteServerContext} from "jopi-rewrite";
 
-let ctx = getRouteServerContext();
+export default function(ctx: RouteServerContext) {
+    ctx.onPOST(async req => {
+        const data = await req.reqBodyAsJson(formSchema);
 
-ctx.onPOST(async req => {
-    const data = await req.reqBodyAsJson(formSchema);
+        console.log("Server received:", data);
 
-    console.log("Server received:", data);
+        // Allow testing the submit button hiding when submitting.
+        await nTimer.tick(1000);
 
-    // Allow testing the submit button hiding when submitting.
-    await nTimer.tick(1000);
-
-    return req.jsonResponse({isOk: true, data});
-});
+        return req.jsonResponse({isOk: true, data});
+    });
+}
