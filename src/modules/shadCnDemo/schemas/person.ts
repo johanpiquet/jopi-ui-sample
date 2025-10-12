eac618e1239821b360f808cfc6bcd287cf685152
Johan PIQUET <johan.piquet@gmail.com>
2025-10-12 11:11:23 +0200

    Correction mode d'import / bundler

--- src/modules/shadCnDemo/schemas/person.ts
+++ src/modules/shadCnDemo/schemas/person.ts
@@ -4,6 +4,11 @@ export const Schema = ns_schema.schema({
     name: ns_schema.string("name"),
 });
 
-ns_schema.registerSchema("d99e8745-9ae0-4de3-a23b-657203caa3fc", Schema);
+ns_schema.registerSchema(
+    "d99e8745-9ae0-4de3-a23b-657203caa3fc", Schema,
+    {
+        title: "Person",
+        description: "A person",
+    });
 
 export type Type = ns_schema.infer<typeof Schema>;
