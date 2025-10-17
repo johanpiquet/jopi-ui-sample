906c12a0d5f27907bd82dbbf1807988c973c3aac
Johan PIQUET <johan.piquet@gmail.com>
2025-10-17 16:09:30 +0200

    Tailwind is now rebuild on each bundler refresh

--- src/modules/shadCnDemo/routes/index.page.tsx
+++ src/modules/shadCnDemo/routes/index.page.tsx
@@ -2,6 +2,7 @@ import {AdminPageLayout} from "jopi-rewrite/uikit";
 
 export default function Page() {
     return <AdminPageLayout>
+        <Test />
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="bg-muted/50 aspect-video rounded-xl" />
             <div className="bg-muted/50 aspect-video rounded-xl" />
@@ -9,4 +10,8 @@ export default function Page() {
         </div>
         <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
     </AdminPageLayout>
+}
+
+function Test() {
+    return <div className="text-green-400">Test 2</div>;
 }
