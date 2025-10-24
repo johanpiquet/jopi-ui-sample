84b47a267028ddb9b5f8cfdba5673d50282fd0ea
Johan PIQUET <johan.piquet@gmail.com>
2025-10-24 17:54:27 +0200

    * Linker moved into jopi-rewrite * Register merged with jk_events

--- src/_jopiLinkerGen/installServer.ts
+++ src/_jopiLinkerGen/installServer.ts
@@ -1,8 +1,6 @@
 
-import {globalRegistry} from "jopi-toolkit/jk_registry";
 import modServerInit1 from "../mod_sample/serverInit.tsx";
 export default function() {
-const registry = globalRegistry;
 
 // @ts-ignore
 modServerInit1(registry);
