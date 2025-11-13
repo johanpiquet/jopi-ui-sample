f40cf8183b7f80e4a2f96a8151d9349cf0d9c207
Johan PIQUET <johan.piquet@gmail.com>
2025-11-13 18:01:30 +0100

    Corrections

--- src/mod_sample/@routes/product/[productName]/page.tsx
+++ src/mod_sample/@routes/product/[productName]/page.tsx
@@ -1,6 +1,6 @@
-import {usePageParams} from "jopi-rewrite/uikit";
+import {useParams} from "jopi-rewrite/uikit";
 
 export default function Product() {
-    const pageParams = usePageParams();
+    const pageParams = useParams();
     return <div>Product {JSON.stringify(pageParams)}</div>;
 }
