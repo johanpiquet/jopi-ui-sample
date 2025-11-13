abceb0d7136003cb7be8c0ff973b9bc1b289c20a
Johan PIQUET <johan.piquet@gmail.com>
2025-11-13 21:24:57 +0100

    JopiRequest.addCookie refactoring

--- src/mod_sample/@routes/login/config.ts
+++ src/mod_sample/@routes/login/config.ts
@@ -1,8 +1,4 @@
 import {RouteConfig} from "jopi-rewrite";
 
 export default function(ctx: RouteConfig) {
-    // GET call will need the user to have
-    // the roles "admin" and "writer".
-    //
-    ctx.onGET.add_requiredRole("admin", "writer");
 }
