f98803c4b1588726c34219883e4ba32914297ded
Johan PIQUET <johan.piquet@gmail.com>
2025-11-13 18:23:07 +0100

    Search params & urls params are now send to the page

--- src/mod_sample/@routes/product/[productName]/page.tsx
+++ src/mod_sample/@routes/product/[productName]/page.tsx
@@ -1,6 +1,9 @@
 import {useParams} from "jopi-rewrite/uikit";
 
-export default function Product() {
-    const pageParams = useParams();
-    return <div>Product {JSON.stringify(pageParams)}</div>;
+export default function Product({params, searchParams}: any) {
+    //const pageParams = useParams();
+    return <div>
+        <div>Product {JSON.stringify(params)}</div>
+        <div>Search params {JSON.stringify(searchParams)}</div>
+    </div>;
 }
