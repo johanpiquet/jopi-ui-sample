69ca2f4312b436cd18b2fb6c294f5c0e98927d1e
Johan PIQUET <johan.piquet@gmail.com>
2025-11-17 11:32:17 +0100

    Added website.fastConfigure_cors and fastConfigure_jwtAuthentification

--- src/index_web.ts
+++ src/index_web.ts
@@ -3,26 +3,21 @@ import myUsers from "./myUsers.json" with { type: "json" };
 
 jopiApp.startApp(import.meta, jopiEasy => {
     jopiEasy.create_creatWebSiteServer()
-        .configure_behaviors()
-            .removeTrailingSlashs(true)
-            .END_configure_behaviors()
 
-        .enable_cors()
-            .add_allowedHost("http://mywebsiteB")
+        .fastConfigure_cors(["http://mywebsiteA", "http://mywebsiteB"])
 
-            .disable_cors()
-            .DONE_enableCors()
+        .fastConfigure_jwtTokenAuth("key", async (info: any) => {
+            if (info.email == "allowed@domain.com") {
+                return {
+                    isOk: true,
 
-        // Add a JWT Token mechanism for user authentification
-        // and user info retrieval.
-        //
-        .enable_jwtTokenAuth()
-            // WARNING: you must change this key!
-            .step_setPrivateKey("my-private-key")
-            .step_setUserStore()
-                .use_simpleLoginPassword()
-                    .addMany(myUsers)
-                    .DONE_use_simpleLoginPassword()
-                .DONE_setUserStore()
-            .DONE_enable_jwtTokenAuth()
-    });
+                    userInfos: {
+                        id: "user1",
+                        roles: ["admin", "writer"]
+                    }
+                }
+            }
+
+            return {isOk: false};
+        })
+});
